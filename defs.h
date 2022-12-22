8d2ee7d687da3074a842c2b31f86f2e911a3aede
Amit Zioni <zioniamit@gmail.com>
2022-12-22 17:45:15 +0100

    Add Byte, Word, SignedByte typedefs

---
+++ defs.h
@@ -0,0 +1,13 @@
+// defs.h - Definitions / typedefs used throughout the program
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdint.h>
+
+typedef uint8_t Byte;
+typedef uint16_t Word;
+typedef int8_t SignedByte;
+
+
+
+#endif
